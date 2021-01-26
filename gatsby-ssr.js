553ba6ea454e4e3a9e2ecb481ebad8ec33824770
Kevin Lee <kevin.ej.lee@gmail.com>
2021-01-25 19:51:38 -0800

    removed store from gatsby ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,10 +1,6 @@
 import React from "react";
-
 import Provider from "./src/react-query";
 
-const createStore = require("./src/state/createStore");
-const store = createStore();
-
 export const wrapRootElement = ({ element }) => {
-  return <Provider store={store}>{element}</Provider>;
+  return <Provider>{element}</Provider>;
 };
