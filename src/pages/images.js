aeddc76abd2195da834a894b74ba8546c3768d81
Kevin E. Lee <kevin.ej.lee@gmail.com>
2021-05-01 17:39:22 -0700

    corrected image paths

--- src/pages/images.js
+++ src/pages/images.js
@@ -4,25 +4,24 @@ import Layout from "../components/layout";
 import altaree from "../images/altaree.jpg";
 import depah from "../images/depah.jpg";
 import napa from "../images/napa.jpg";
-import image0 from "../images/jokpah.jpg";
-import image1 from "../images/jokpah.jpg";
-import image2 from "../images/image2.jpg";
-import image3 from "../images/image3.jpg";
-import image4 from "../images/image4.jpg";
-import image5 from "../images/image5.jpg";
-import image6 from "../images/image6.jpg";
-import image7 from "../images/image7.jpg";
-import image8 from "../images/image8.jpg";
-import image9 from "../images/image9.jpg";
-import image10 from "../images/image10.jpg";
-import image11 from "../images/image11.jpg";
-import image12 from "../images/image12.jpg";
-import image13 from "../images/image13.jpg";
-import image14 from "../images/image14.jpg";
-import image15 from "../images/image15.jpg";
-import image16 from "../images/image16.jpg";
-import image17 from "../images/image17.jpg";
-
+import image0 from "../images/kimchi-family/image0.jpeg";
+import image1 from "../images/kimchi-family/image1.jpeg";
+import image2 from "../images/kimchi-family/image2.jpeg";
+import image3 from "../images/kimchi-family/image3.jpeg";
+import image4 from "../images/kimchi-family/image4.jpeg";
+import image5 from "../images/kimchi-family/image5.jpeg";
+import image6 from "../images/kimchi-family/image6.jpeg";
+import image7 from "../images/kimchi-family/image7.jpeg";
+import image8 from "../images/kimchi-family/image8.jpeg";
+import image9 from "../images/kimchi-family/image9.jpeg";
+import image10 from "../images/kimchi-family/image10.jpeg";
+import image11 from "../images/kimchi-family/image11.jpeg";
+import image12 from "../images/kimchi-family/image12.jpeg";
+import image13 from "../images/kimchi-family/image13.jpeg";
+import image14 from "../images/kimchi-family/image14.jpeg";
+import image15 from "../images/kimchi-family/image15.jpeg";
+import image16 from "../images/kimchi-family/image16.jpeg";
+import image17 from "../images/kimchi-family/image17.jpeg";
 
 function ImagesPage() {
   return (
@@ -32,27 +31,24 @@ function ImagesPage() {
         <img src={depah} />
         <img src={napa} />
         <img src={jokpah} />
-        <img src={image0}/>
-        <img src={image1}/>
-        <img src={image2}/>
-        <img src={image3}/>
-        <img src={image4}/>
-        <img src={image5}/>
-        <img src={image6}/>
-        <img src={image7}/>
-        <img src={image8}/>
-        <img src={image9}/>
-        <img src={image10}/>
-        <img src={image11}/>
-        <img src={image12}/>
-        <img src={image13}/>
-        <img src={image14}/>
-        <img src={image15}/>
-        <img src={image16}/>
-        <img src={image17}/>
-        
-
-        
+        <img src={image0} />
+        <img src={image1} />
+        <img src={image2} />
+        <img src={image3} />
+        <img src={image4} />
+        <img src={image5} />
+        <img src={image6} />
+        <img src={image7} />
+        <img src={image8} />
+        <img src={image9} />
+        <img src={image10} />
+        <img src={image11} />
+        <img src={image12} />
+        <img src={image13} />
+        <img src={image14} />
+        <img src={image15} />
+        <img src={image16} />
+        <img src={image17} />
       </section>
     </Layout>
   );
