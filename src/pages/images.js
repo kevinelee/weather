4fa032a2b1180b7f101d5b46df08615a8822829f
Kevin E. Lee <kevin.ej.lee@gmail.com>
2021-05-01 17:35:03 -0700

    added images

--- src/pages/images.js
+++ src/pages/images.js
@@ -4,7 +4,25 @@ import Layout from "../components/layout";
 import altaree from "../images/altaree.jpg";
 import depah from "../images/depah.jpg";
 import napa from "../images/napa.jpg";
-import jokpah from "../images/jokpah.jpg";
+import image0 from "../images/jokpah.jpg";
+import image1 from "../images/jokpah.jpg";
+import image2 from "../images/image2.jpg";
+import image3 from "../images/image3.jpg";
+import image4 from "../images/image4.jpg";
+import image5 from "../images/image5.jpg";
+import image6 from "../images/image6.jpg";
+import image7 from "../images/image7.jpg";
+import image8 from "../images/image8.jpg";
+import image9 from "../images/image9.jpg";
+import image10 from "../images/image10.jpg";
+import image11 from "../images/image11.jpg";
+import image12 from "../images/image12.jpg";
+import image13 from "../images/image13.jpg";
+import image14 from "../images/image14.jpg";
+import image15 from "../images/image15.jpg";
+import image16 from "../images/image16.jpg";
+import image17 from "../images/image17.jpg";
+
 
 function ImagesPage() {
   return (
@@ -14,6 +32,27 @@ function ImagesPage() {
         <img src={depah} />
         <img src={napa} />
         <img src={jokpah} />
+        <img src={image0}/>
+        <img src={image1}/>
+        <img src={image2}/>
+        <img src={image3}/>
+        <img src={image4}/>
+        <img src={image5}/>
+        <img src={image6}/>
+        <img src={image7}/>
+        <img src={image8}/>
+        <img src={image9}/>
+        <img src={image10}/>
+        <img src={image11}/>
+        <img src={image12}/>
+        <img src={image13}/>
+        <img src={image14}/>
+        <img src={image15}/>
+        <img src={image16}/>
+        <img src={image17}/>
+        
+
+        
       </section>
     </Layout>
   );
